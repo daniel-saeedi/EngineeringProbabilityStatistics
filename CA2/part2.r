0b674561c34d6bb4b3f9fa3943b3491e4e143f9a
Daniel Saeedi <saeedi.danial@gmail.com>
2020-12-06 01:45:25 +0330

    CA2: part 2

--- CA2/part2.r
+++ CA2/part2.r
@@ -1,5 +1,27 @@
-# Create uniform distribution between range -1 to 1 
-x <- runif(20, min = -1, max = 1) 
-  
-# Print distibution 
-print(x)
+#Probability of success
+p <- 0.6
+#Number of experiments
+n <- 10;
+Binomial <- function() {
+	Y <- 0;
+	for(i in 1:n)
+	{
+		#U~Uniform(0,1)
+		U <- runif(1, min = 0, max = 1)
+		#X~Ber(p)
+		X <- (U < p)
+		#Y~binomial(n=10,p=0.6)
+		Y <- Y + X
+	}
+	return(Y)
+}
+#numbef of samples
+num_samples <- 100
+samples <- c()
+for(i in 1:num_samples)
+{
+	new_sample <- Binomial();
+	samples <- c(samples,new_sample);
+}
+mean(samples)
+var(samples)
